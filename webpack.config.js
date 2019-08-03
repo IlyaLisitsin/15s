0f035c7ac02783ce370c7522489ff6ff606fbf75
Illia Sauchuk <ilyas.sporichev@gmail.com>
2019-08-03 19:31:05 +0300

    initial

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env, arg) => {
+    const { mode } = arg;
+
+    const isProd = mode === 'production';
+
+    return{
+        entry: {
+            'main': __dirname + '/src/js/main',
+        },
+        output: {
+            path: __dirname +  '/public',
+            publicPath: isProd ? './' : '/',
+            chunkFilename: '[name].js',
+            filename: '[name].js'
+        },
+        optimization: {
+            minimize: isProd
+        },
+        watch: true,
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['es2015', 'es2017']
+                        }
+                    }
+                },
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                inject: true,
+                template: __dirname + '/src/index.html',
+                filename: 'index.html'
+            }),
+        ],
+        devServer: {
+            contentBase: './public',
+            port: 3000,
+        },
+        resolve: {
+            extensions: ['.js']
+        }
+    }
+}
